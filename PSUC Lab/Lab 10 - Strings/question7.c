1fc2383eb1e819707329271be8a799fa30532f4c
Vansh Sharma <developer.viktorexe@gmail.com>
2025-04-19 18:04:13 +0530

    Update question7.c

--- PSUC Lab/Lab 10 - Strings/question7.c
+++ PSUC Lab/Lab 10 - Strings/question7.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
+
 int main() {
-    char s[100], sub[100];
-    gets(s);
-    gets(sub);
-    int i, j, found;
-    for(i=0; s[i]; i++) {
-        found = 1;
-        for(j=0; sub[j]; j++)
-            if(s[i+j] != sub[j]) found = 0;
-        if(found) break;
+    char str[100], sub[100];
+    fgets(str, 100, stdin);
+    fgets(sub, 100, stdin);
+
+    int found = 0;
+    for (int i = 0; str[i]; i++) {
+        int match = 1;
+        for (int j = 0; sub[j] && sub[j] != '\n'; j++) {
+            if (str[i + j] != sub[j]) {
+                match = 0;
+                break;
+            }
+        }
+        if (match) {
+            found = 1;
+            break;
+        }
     }
-    printf(found ? "Found" : "Not found");
+
+    printf(found ? "Substring found" : "Substring not found");
     return 0;
 }
