eab220adccba69a99d88040074f21c4dc7a7812c
Vansh Sharma <developer.viktorexe@gmail.com>
2025-04-19 18:03:46 +0530

    Update question5.c

--- PSUC Lab/Lab 10 - Strings/question5.c
+++ PSUC Lab/Lab 10 - Strings/question5.c
@@ -2,24 +2,16 @@
 #include <string.h>
 
 int main() {
-    char str1[100], str2[100];
+    char str[100];
+    fgets(str, 100, stdin);
+    int len = strlen(str) - 1; // -1 to exclude '\n'
 
-    printf("Enter first string: ");
-    fgets(str1, sizeof(str1), stdin);
-    str1[strcspn(str1, "\n")] = '\0'; // Remove newline character
+    for (int i = 0; i < len / 2; i++) {
+        char temp = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = temp;
+    }
 
-    printf("Enter second string: ");
-    fgets(str2, sizeof(str2), stdin);
-    str2[strcspn(str2, "\n")] = '\0'; // Remove newline character
-
-    int result = strcmp(str1, str2);
-
-    if (result == 0)
-        printf("The strings are equal.\n");
-    else if (result < 0)
-        printf("String 1 comes before String 2.\n");
-    else
-        printf("String 1 comes after String 2.\n");
-
+    printf("%s", str);
     return 0;
 }
